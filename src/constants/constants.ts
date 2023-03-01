ed2c2f69e9c909ad57f7f00816d16f819b905638
Ilya030290 <anoshko90@mail.ru>
2023-03-01 10:19:50 +0300

    feat: add constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,10 @@
+export const Profile = 'profile';
+export const Bookings = 'bookings';
+export const Places = 'places';
+export const New = 'new';
+export const PATH = {
+  PROFILE: '/profile',
+  BOOKINGS: '/profile/bookings',
+  PLACES: '/profile/places',
+  ADD_NEW_PLACE: '/profile/places/new',
+};
